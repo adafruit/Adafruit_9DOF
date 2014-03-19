3e87dfa6af8925ff9b722b4d86ae5a4ae4272e0d
Kevin Townsend <contact@microbuilder.eu>
2014-03-19 13:40:26 +0100

    Added AHRS sample code

--- Adafruit_9DOF.cpp
+++ Adafruit_9DOF.cpp
@@ -277,3 +277,82 @@ bool Adafruit_9DOF::magGetOrientation(sensors_axis_t axis, sensors_event_t *even
 
   return true;
 }
+
+/**************************************************************************/
+/*!
+    @brief  Populates the .roll/.pitch/.heading fields in the sensors_vec_t
+            struct with the right angular data (in degree).
+
+            The starting position is set by placing the object flat and
+            pointing northwards (Z-axis pointing upward and X-axis pointing
+            northwards).
+
+            The orientation of the object can be modeled as resulting from
+            3 consecutive rotations in turn: heading (Z-axis), pitch (Y-axis),
+            and roll (X-axis) applied to the starting position.
+
+
+    @param  accel_event   The sensors_event_t variable containing the
+                          data from the accelerometer
+
+    @param  mag_event     The sensors_event_t variable containing the
+                          data from the magnetometer
+
+    @param  orientation   The sensors_vec_t object that will have it's
+                          .roll, .pitch and .heading fields populated
+*/
+/**************************************************************************/
+bool Adafruit_9DOF::fusionGetOrientation(sensors_event_t *accel_event, sensors_event_t *mag_event, sensors_vec_t *orientation)
+{
+  /* Make sure the input is valid, not null, etc. */
+  if ( accel_event  == NULL) return false;
+  if ( mag_event    == NULL) return false;
+  if ( orientation  == NULL) return false;
+
+  float const PI_F = 3.14159265F;
+
+  /* roll: Rotation around the X-axis. -180 <= roll <= 180                                          */
+  /* a positive roll angle is defined to be a clockwise rotation about the positive X-axis          */
+  /*                                                                                                */
+  /*                    y                                                                           */
+  /*      roll = atan2(---)                                                                         */
+  /*                    z                                                                           */
+  /*                                                                                                */
+  /* where:  y, z are returned value from accelerometer sensor                                      */
+  orientation->roll = (float)atan2(accel_event->acceleration.y, accel_event->acceleration.z);
+
+  /* pitch: Rotation around the Y-axis. -180 <= roll <= 180                                         */
+  /* a positive pitch angle is defined to be a clockwise rotation about the positive Y-axis         */
+  /*                                                                                                */
+  /*                                 -x                                                             */
+  /*      pitch = atan(-------------------------------)                                             */
+  /*                    y * sin(roll) + z * cos(roll)                                               */
+  /*                                                                                                */
+  /* where:  x, y, z are returned value from accelerometer sensor                                   */
+  if (accel_event->acceleration.y * sin(orientation->roll) + accel_event->acceleration.z * cos(orientation->roll) == 0)
+    orientation->pitch = accel_event->acceleration.x > 0 ? (PI_F / 2) : (-PI_F / 2);
+  else
+    orientation->pitch = (float)atan(-accel_event->acceleration.x / (accel_event->acceleration.y * sin(orientation->roll) + \
+                                                                     accel_event->acceleration.z * cos(orientation->roll)));
+
+  /* heading: Rotation around the Z-axis. -180 <= roll <= 180                                       */
+  /* a positive heading angle is defined to be a clockwise rotation about the positive Z-axis       */
+  /*                                                                                                */
+  /*                                       z * sin(roll) - y * cos(roll)                            */
+  /*   heading = atan2(--------------------------------------------------------------------------)  */
+  /*                    x * cos(pitch) + y * sin(pitch) * sin(roll) + z * sin(pitch) * cos(roll))   */
+  /*                                                                                                */
+  /* where:  x, y, z are returned value from magnetometer sensor                                    */
+  orientation->heading = (float)atan2(mag_event->magnetic.z * sin(orientation->roll) - mag_event->magnetic.y * cos(orientation->roll), \
+                                      mag_event->magnetic.x * cos(orientation->pitch) + \
+                                      mag_event->magnetic.y * sin(orientation->pitch) * sin(orientation->roll) + \
+                                      mag_event->magnetic.z * sin(orientation->pitch) * cos(orientation->roll));
+
+
+  /* Convert angular data to degree */
+  orientation->roll = orientation->roll * 180 / PI_F;
+  orientation->pitch = orientation->pitch * 180 / PI_F;
+  orientation->heading = orientation->heading * 180 / PI_F;
+
+  return true;
+}
---
+++ ahrs/ahrs.pde
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_9DOF.h>
+
+/* Assign a unique ID to the sensors */
+Adafruit_9DOF                 dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+
+/**************************************************************************/
+/*!
+    @brief  Initialises all the sensors used by this example
+*/
+/**************************************************************************/
+void initSensors()
+{
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+/**************************************************************************/
+/*!
+
+*/
+/**************************************************************************/
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading Example")); Serial.println("");
+  
+  /* Initialise the sensors */
+  initSensors();
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly check the roll/pitch/heading/altitude/temperature
+*/
+/**************************************************************************/
+void loop(void)
+{
+  sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_vec_t   orientation;
+
+  /* Read the accelerometer and magnetometer */
+  accel.getEvent(&accel_event);
+  mag.getEvent(&mag_event);
+
+  /* Use the new fusionGetOrientation function to merge accel/mag data */  
+  if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation))
+  {
+    /* 'orientation' should have valid .roll and .pitch fields */
+    Serial.print(F("Orientation: "));
+    Serial.print(orientation.roll);
+    Serial.print(F(" "));
+    Serial.print(orientation.pitch);
+    Serial.print(F(" "));
+    Serial.print(orientation.heading);
+    Serial.println(F(""));
+  }
+  
+  delay(100);
+}
